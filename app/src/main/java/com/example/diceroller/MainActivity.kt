0bf965d31b4d38b3bfffca6c51161f18808bc271
camila <cm.maschion@gmail.com>
2022-03-15 12:05:19 +0000

    update dice roller

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,32 +1,41 @@
 package com.example.diceroller
 
-import android.media.Image
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import java.util.*
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var diceImage : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener{
-            rollDice()
-        }
 
-        diceImage = findViewById(R.id.dice_image)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+        // Do a dice roll when the app starts
+        rollDice()
     }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
 
     private fun rollDice() {
 
-        val randomInt = Random().nextInt(6) + 1
-        val drawableResource = when ( randomInt) {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Update the screen with the dice roll
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+
+        val drawableResource = when (diceRoll) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -34,6 +43,7 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage.setImageResource((drawableResource))
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
