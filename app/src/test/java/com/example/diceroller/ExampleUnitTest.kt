8bc9a0338019c3976ae9bf2551eccb9465a184e5
camila <cm.maschion@gmail.com>
2022-03-17 11:23:45 +0000

    test

--- app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -1,17 +1,27 @@
 package com.example.diceroller
 
+import org.junit.Assert.assertTrue
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
+ *
+ *
+ * As funções de teste precisam ter a anotação @ Test, importada da biblioteca org.junit.test.
+ * A anotação @Test informa ao compilador que o método a seguir é um teste, o que permite que ele seja executado como tal.
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
-    }
+   fun generate_numbers(){
+       val dice = Dice(6)
+        val rollResult = dice.roll()
+        assertTrue("The value of rollResult was not between 1 and 6", rollResult in 1..6)
+
+   }
 }
+
+
+//A lib JUnit aciona os testes de unidade e permite marcar um código como sendo
+//de teste para que ele possa ser compilado e executado de forma a testar o código de apps.
